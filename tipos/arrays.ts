0b1403ccd6b54cd8511fc994efbb284dbe33be80
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-03-13 23:53:42 -0400

    arrays

---
+++ tipos/arrays.ts
@@ -0,0 +1,14 @@
+(() => {
+
+    // const numbers: (string | number | boolean)[] = [1,2,3,4,5,6,7,8,9];
+    const numbers: number[] = [1,2,3,4,5,6,7,8,9];
+
+    const villians = ['Omega rojo', 'Dormamu', 'Duende Verede'];
+
+    villians.forEach((v) => console.log(v.toUpperCase()));
+
+    // numbers.push(true);
+
+    console.log(numbers)
+
+})()
