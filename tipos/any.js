68f84169fede57b6d315eb520748b949756448a2
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-03-15 01:06:03 -0400

    tuples

--- tipos/any.js
+++ tipos/any.js
@@ -7,6 +7,7 @@
     //   console.log(avenger.charAt(0));
     console.log(avenger.charAt(0));
     avenger = 150.232123;
+    //   console.log(avenger.toFixed(2));
     console.log(avenger.toFixed(2));
     console.log(exists);
     console.log(power);
---
+++ tipos/tuples.js
@@ -0,0 +1,7 @@
+"use strict";
+(() => {
+    const hero = ['Dr Strange', 100];
+    hero[0] = 'Iron Man';
+    hero[1] = 50;
+    console.log(hero);
+})();
