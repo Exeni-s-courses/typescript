92da2b78d5522f47bc0e716528163bb5ea55ae90
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-03-21 18:41:19 -0400

    functions args optional

---
+++ funciones/args-optional.js
@@ -0,0 +1,10 @@
+"use strict";
+(() => {
+    const fullName = (firstName, lastName) => {
+        return `${firstName} ${lastName || '----'}`;
+    };
+    let noName;
+    const name = fullName("Tony", "Stark");
+    //   const name = fullName(noName, "Stark");
+    console.log(name);
+})();
