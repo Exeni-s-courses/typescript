3e9457c264097aa7fc482f6ac69dae61f3f28259
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-03-23 23:35:50 -0400

    objetos

---
+++ objetos/objects.js
@@ -0,0 +1,16 @@
+"use strict";
+(() => {
+    let flash = {
+        name: "Bassy Allen",
+        age: 24,
+        powers: ["super velocidad", "viajar en el tiempo"],
+    };
+    flash = {
+        name: "Clark Kent",
+        age: 60,
+        powers: ["super fuerza"],
+        // getNombre() {
+        //   return this.name;
+        // },
+    };
+})();
